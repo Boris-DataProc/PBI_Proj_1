d496fda27b85a53fae94bbf6538b14851ab90490
Boris-DataProc <79475536+Boris-DataProc@users.noreply.github.com>
2023-07-30 21:41:39 +0300

    Add files via upload
    SQL queries

---
+++ DIM_Calendar.sql
@@ -0,0 +1,15 @@
+-- Cleared DIM_Date Table --
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+  --[DayNumberOfWeek], 
+  [EnglishDayNameOfWeek] AS Day, 
+  [EnglishMonthName] AS Month, 
+  Left([EnglishMonthName], 3) AS MonthShort,   -- Useful for front end date navigation and front end graphs.
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year  
+FROM 
+ [AdventureWorksDW2019].[dbo].[DimDate]
+--WHERE 
+  --CalendarYear >= 2019
---
+++ Fact_Internet_Sales.sql
@@ -0,0 +1,15 @@
+-- Cleared FACT_InternetSales Table --
+SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey], 
+  [SalesOrderNumber], 
+  [SalesAmount]
+FROM 
+  [AdventureWorksDW2019].[dbo].[FactInternetSales]
+WHERE 
+  LEFT (OrderDateKey, 4) >= YEAR(GETDATE()) -2 -- Ensures we always only bring two years of date from extraction.
+ORDER BY
+  OrderDateKey ASC
